7e8a65d651aadd30fb12429c769e80ebc30042ed
Scott Johnson <jaywir3@gmail.com>
2015-05-25 11:46:23 -0500

    (#1): Add resource bundle to plugin to allow for string externalization.

---
+++ src/com/glasstowerstudios/intellij/intellitagger/resources/IntellitaggerBundle.java
@@ -0,0 +1,38 @@
+package com.glasstowerstudios.intellij.intellitagger.resources;
+
+import com.intellij.CommonBundle;
+import com.intellij.reference.SoftReference;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+import java.lang.ref.Reference;
+import java.util.ResourceBundle;
+
+/**
+ * Resource bundle for Intellitagger IDEA plugin.
+ */
+public class IntellitaggerBundle {
+    private static Reference<ResourceBundle> mBundle;
+    @NonNls
+    private static final String BUNDLE = "resources.intellitagger";
+
+    private IntellitaggerBundle() {
+    }
+
+    public static String message(@NotNull @NonNls @PropertyKey(
+            resourceBundle = "resources.intellitagger"
+    )String key, @NotNull Object... params) {
+        return CommonBundle.message(getBundle(), key, params);
+    }
+
+    private static ResourceBundle getBundle() {
+        ResourceBundle bundle = SoftReference.dereference(mBundle);
+        if (bundle == null) {
+            bundle = ResourceBundle.getBundle(BUNDLE);
+            mBundle = new java.lang.ref.SoftReference(bundle);
+        }
+
+        return bundle;
+    }
+}
